88f26e4e39c802040ed04d2d8ca21b49f9cd6d12
farid froozan <froozan@yahoo.com>
2021-02-26 21:44:29 +0330

    remove extra methods

--- src/Api.php
+++ src/Api.php
@@ -13,24 +13,6 @@ class Api{
     public static $data=null,$message=null,$errors=null,$code=null,$instance=null,$extra=null;
 
     /**
-     * Return a new JSON response from the application.
-     *
-     * @param  string|array  $data
-     * @param  int  $status
-     * @param  array  $headers
-     * @param  int  $options
-     * @return \Illuminate\Http\JsonResponse
-     */
-
-    public function json($data = [], $status = 200, array $headers = [], $options = 0)
-    {
-        if ($data instanceof Arrayable && ! $data instanceof JsonSerializable) {
-            $data = $data->toArray();
-        }
-        return new JsonResponse($data, $status, $headers, $options);
-    }
-
-    /**
      * response
      * make a custom response
      *
